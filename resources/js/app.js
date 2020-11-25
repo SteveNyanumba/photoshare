69403de30fa7cb70636ec170856f94f4d90d6646
Steve Nyanumba <reach@stevenyanumba.com>
2020-11-25 18:22:38 +0300

    Scaffolded the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+require('./bootstrap')
+require('jquery.easing')
+require('isotope-layout')
+const AOS = require('aos');
+window.AOS = AOS
+
+require('venobox/venobox/venobox')
+require('./main')
